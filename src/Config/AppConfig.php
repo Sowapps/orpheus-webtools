29527839d75722c70c4a75cdfade450b33faab34
Loenix <florent@sowapps.com>
2020-03-18 15:08:52 +0100

    Add typed declaration of config Add meta data to config json with BC (auto convert old format)

--- src/Config/AppConfig.php
+++ src/Config/AppConfig.php
@@ -5,96 +5,47 @@
 
 namespace Orpheus\Config;
 
+use Exception;
+
 /**
  * The AppConfig class
- * 
+ *
  * Store application configuration in a file
- * 
+ *
  * @author Florent Hazard <contact@sowapps.com>
- * 
+ *
  */
 class AppConfig {
 	
-	/**
-	 * The path to config file
-	 * 
-	 * @var string
-	 */
+	const VERSION = '2';
+	CONST TYPE_STRING = 'simple';
+	CONST TYPE_BOOLEAN = 'boolean';
+	CONST TYPE_LONG_TEXT = 'text';
+	CONST DEFAULT_TYPE = self::TYPE_STRING;
+	
+	/** @var static */
+	protected static $instance;
+	
+	/** @var string */
 	protected $path;
 	
-	/**
-	 * The config data
-	 * 
-	 * @var array
-	 */
+	/** @var array */
 	protected $data;
+	
+	/** @var array */
+	protected $meta;
+	
+	/** @var bool */
+	protected $changed;
 	
 	/**
 	 * Constructor
 	 */
 	protected function __construct() {
-		$this->path = defined('STOREPATH') ? STOREPATH.'config.json' : null;
-		$this->data	= array();
+		$this->path = defined('STOREPATH') ? STOREPATH . 'config.json' : null;
+		$this->meta = [];
+		$this->data = [];
 		$this->loadSmartly();
-	}
-	
-	/**
-	 * Get it as array
-	 * 
-	 * @return array
-	 */
-	public function asArray() {
-		return $this->data;
-	}
-	
-	/**
-	 * Set $key if not yet set
-	 * 
-	 * @param string $key
-	 * @param mixed $default
-	 */
-	public function preset($key, $default) {
-		if( !$this->has($key) ) {
-			$this->set($key, $default);
-		}
-	}
-	
-	/**
-	 * Test if config has $key
-	 * 
-	 * @param string $key
-	 */
-	public function has($key) {
-		return isset($this->data[$key]);
-	}
-	
-	/**
-	 * Get value of $key or $default
-	 * 
-	 * @param string $key
-	 * @param mixed $default
-	 */
-	public function get($key, $default=null) {
-		return $this->has($key) ? $this->data[$key] : $default;
-	}
-
-	/**
-	 * Set the $value of $key
-	 *
-	 * @param string $key
-	 * @param mixed $value
-	 */
-	public function set($key, $value) {
-		$this->data[$key] = $value;
-	}
-	
-	/**
-	 * Test if config has $key
-	 *
-	 * @param string $key
-	 */
-	public function remove($key) {
-		unset($this->data[$key]);
 	}
 	
 	/**
@@ -108,57 +59,139 @@ class AppConfig {
 	
 	/**
 	 * Load config from filesystem
-	 * 
-	 * @throws \Exception
+	 *
+	 * @throws Exception
 	 */
 	public function load() {
 		if( !$this->path ) {
-			throw new \Exception('Unable to load AppConfig from undefined path');
+			throw new Exception('Unable to load AppConfig from undefined path');
 		}
-		$this->data	= json_decode(file_get_contents($this->path), true);
+		$jsonConfig = (object) json_decode(file_get_contents($this->path), true);
+		if( isset($jsonConfig->data) ) {
+			// New format
+			$this->meta = $jsonConfig->meta;
+			$this->data = $jsonConfig->data;
+		} else {
+			// BC with old format
+			$this->data = $jsonConfig;
+		}
+	}
+	
+	/**
+	 * Get it as array
+	 *
+	 * @return array
+	 */
+	public function asArray() {
+		return $this->data;
+	}
+	
+	/**
+	 * Set $key if not yet set
+	 *
+	 * @param string $key
+	 * @param mixed $default
+	 * @param string $type
+	 * @return bool
+	 */
+	public function preset($key, $default, $type = null) {
+		$changed = false;
+		if( $type || !isset($this->meta[$key]) ) {
+			$type = $type ?: self::DEFAULT_TYPE;
+			if( !isset($this->meta[$key]) || $this->meta[$key] !== $type ) {
+				$this->meta[$key] = $type ?: self::DEFAULT_TYPE;
+				$this->changed = true;
+				$changed = true;
+			}
+		}
+		if( !$this->has($key) ) {
+			$this->set($key, $default);
+			$changed = true;
+		}
+		return $changed;
+	}
+	
+	/**
+	 * Test if config has $key
+	 *
+	 * @param string $key
+	 * @return bool
+	 */
+	public function has($key) {
+		return isset($this->data[$key]);
+	}
+	
+	/**
+	 * Set the $value of $key
+	 *
+	 * @param string $key
+	 * @param mixed $value
+	 * @return bool
+	 */
+	public function set($key, $value) {
+		if( array_key_exists($key, $this->data) && $this->data[$key] === $value ) {
+			return false;
+		}
+		$this->data[$key] = $value;
+		$this->changed = true;
+		return true;
+	}
+	
+	/**
+	 * Get value by key
+	 *
+	 * @param string $key The key to look for
+	 * @param mixed|null $default The default value if key is not set
+	 * @return mixed|null
+	 */
+	public function get($key, $default = null) {
+		return $this->has($key) ? $this->data[$key] : $default;
+	}
+	
+	/**
+	 * Test if config has $key
+	 *
+	 * @param string $key
+	 */
+	public function remove($key) {
+		unset($this->data[$key]);
+	}
+	
+	/**
+	 * Destructor auto save configuration
+	 */
+	public function __destruct() {
+		if( $this->changed ) {
+			$this->save();
+		}
 	}
 	
 	/**
 	 * Save config into the filesystem
-	 * 
+	 *
 	 * @return int
 	 */
 	public function save() {
-		return file_put_contents($this->path, json_encode($this->data));
+		return file_put_contents($this->path, json_encode([
+			'meta'    => $this->meta,
+			'data'    => $this->data,
+			'version' => static::VERSION,
+		]));
 	}
 	
 	/**
-	 * Global AppConfig
-	 * 
-	 * @var AppConfig
+	 * Get the type of $key
+	 *
+	 * @param $key
+	 * @return string
 	 */
-	protected static $instance;
-	
-	/**
-	 * Alias for getInstance()
-	 * 
-	 * @return AppConfig
-	 * @see getInstance()
-	 */
-	public static function instance() {
-		return static::getInstance();
-	}
-	
-	/**
-	 * Get main instance
-	 * 
-	 * @return AppConfig
-	 */
-	public static function getInstance() {
-		if( !static::$instance ) {
-			static::$instance = new static();
-		}
-		return static::$instance;
+	public function getType($key) {
+		return isset($this->meta[$key]) ? $this->meta[$key] : self::DEFAULT_TYPE;
 	}
 	
 	/**
 	 * Get the path
-	 * 
+	 *
 	 * @return string
 	 */
 	public function getPath() {
@@ -178,7 +211,7 @@ class AppConfig {
 	
 	/**
 	 * Get the data
-	 * 
+	 *
 	 * @return array
 	 */
 	public function getData() {
@@ -196,6 +229,26 @@ class AppConfig {
 		return $this;
 	}
 	
+	/**
+	 * Alias for getInstance()
+	 *
+	 * @return AppConfig
+	 * @see getInstance()
+	 */
+	public static function instance() {
+		return static::getInstance();
+	}
 	
+	/**
+	 * Get main instance
+	 *
+	 * @return AppConfig
+	 */
+	public static function getInstance() {
+		if( !static::$instance ) {
+			static::$instance = new static();
+		}
+		return static::$instance;
+	}
 	
 }
